71b65a16040b63e08dfd60f97bc92110393da743
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2025-03-17 16:18:49 -0700

    start create workout form

--- src/components/Workout/WorkoutCreate/WorkoutCreate.jsx
+++ src/components/Workout/WorkoutCreate/WorkoutCreate.jsx
@@ -1,3 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './WorkoutCreate.module.css';
+import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
+
 export default function WorkoutCreate() {
-  return <div></div>;
+  return (
+    <div className={styles.createWorkout}>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className={styles.createWorkout}
+      >
+        <div className='workoutHeader'>
+          {/* Workout Name Input */}
+          <label htmlFor='workoutName' hidden>
+            Workout Name
+          </label>
+          <input type='text' placeholder='Workout Name' />
+          {/* Finish Button */}
+          <button type='submit' className={styles.createBtn}>
+            <FontAwesomeIcon icon={faSquareCheck} />
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
