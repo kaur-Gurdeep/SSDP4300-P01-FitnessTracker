67206d334bbff2abd1aaaca59ceee770982f53cc
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2025-03-17 16:25:19 -0700

    create temp folder with dummy data

---
+++ temp/data/workout.js
@@ -0,0 +1,69 @@
+const workout = {
+  id: 1,
+  date: '2025-03-16',
+  name: 'Workout 1',
+  exercises: [
+    {
+      id: 1,
+      name: 'Exercise 1',
+      type: 'strength',
+      duration: 30,
+      sets: [
+        {
+          reps: 10,
+          weight: 100,
+        },
+        {
+          reps: 10,
+          weight: 100,
+        },
+        {
+          reps: 10,
+          weight: 100,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Exercise 2',
+      type: 'strength',
+      duration: 30,
+      sets: [
+        {
+          reps: 10,
+          weight: 100,
+        },
+        {
+          reps: 10,
+          weight: 100,
+        },
+        {
+          reps: 10,
+          weight: 100,
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: 'Exercise 3',
+      type: 'cardio',
+      duration: 30,
+      sets: [
+        {
+          distance: 1000,
+          time: 30,
+        },
+        {
+          distance: 2000,
+          time: 30,
+        },
+        {
+          distance: 1000,
+          time: 30,
+        },
+      ],
+    },
+  ],
+};
+
+export default workout;
