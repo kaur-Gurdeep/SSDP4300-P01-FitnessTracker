aff9a95479d8280e814afc677e53e029d6e0d444
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2025-03-17 16:06:31 -0700

    add pathing to workout and workoutcreate

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'; // Global styles
 import Home from './components/Layout/Home';
 import Footer from './components/Layout/Footer';
 import Header from './components/Layout/Header';
-import Login from "./components/Auth/Login";
-import Register from "./components/Auth/Register";
-import About from "./components/About";
-import Contact from "./components/Contact";
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import About from './components/About';
+import Contact from './components/Contact';
+import WorkoutSummary from './components/Workout/WorkoutSummary/WorkoutSummary';
+import WorkoutCreate from './components/Workout/WorkoutCreate/WorkoutCreate';
 
 function App() {
-  
   return (
     <Router>
-    <div className="appContainer">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </div>
+      <div className='appContainer'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/workouts' element={<WorkoutSummary />} />
+          <Route path='/workout/create' element={<WorkoutCreate />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import './layout.css';
-import Logo from "../../assets/logo.png";
-import Bars from "../../assets/bars.png";
-import { Link } from "react-router-dom";
-
+import Logo from '../../assets/logo.png';
+import Bars from '../../assets/bars.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
@@ -12,48 +11,54 @@ const Header = () => {
   // Dynamically update screen size
   useEffect(() => {
     const handleResize = () => setIsMobile(window.innerWidth <= 768);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return (
-        <div className="header">
-          <img src={Logo} alt="fitness tracker logo" className="logo" />
+    <div className='header'>
+      <img src={Logo} alt='fitness tracker logo' className='logo' />
 
-          {menuOpened === false && isMobile ? (
-            <div
-              style={{
-                cursor: "pointer",
-                backgroundColor: "var(--appColor)",
-                padding: "0.5rem",
-                borderRadius: "5px",
-              }}
-              onClick={() => setMenuOpened(true)}
-            >
-              <img
-                src={Bars}
-                alt="hamburger"
-                style={{
-                  width: "1.5rem",
-                  height: "1.5rem",
-                  maxHeight: "100%", 
-                }}
-              />
-            </div>
-          ) : (
-            <ul className="header-menu">
-              <li onClick={() => setMenuOpened(false)}>
-                <Link to="/">Home</Link>
-              </li>
-              <li onClick={() => setMenuOpened(false)}>
-                <Link to="/about">About</Link>
-              </li>
-              <li onClick={() => setMenuOpened(false)}>
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-          )}
-      </div>
+      {menuOpened === false && isMobile ? (
+        <div
+          style={{
+            cursor: 'pointer',
+            backgroundColor: 'var(--appColor)',
+            padding: '0.5rem',
+            borderRadius: '5px',
+          }}
+          onClick={() => setMenuOpened(true)}
+        >
+          <img
+            src={Bars}
+            alt='hamburger'
+            style={{
+              width: '1.5rem',
+              height: '1.5rem',
+              maxHeight: '100%',
+            }}
+          />
+        </div>
+      ) : (
+        <ul className='header-menu'>
+          <li onClick={() => setMenuOpened(false)}>
+            <Link to='/'>Home</Link>
+          </li>
+          <li onClick={() => setMenuOpened(false)}>
+            <Link to='/about'>About</Link>
+          </li>
+          <li onClick={() => setMenuOpened(false)}>
+            <Link to='/contact'>Contact</Link>
+          </li>
+          <li onClick={() => setMenuOpened(false)}>
+            <Link to='/workouts'>Workouts</Link>
+          </li>
+          <li onClick={() => setMenuOpened(false)}>
+            <Link to='/workout/create'>Create Workout</Link>
+          </li>
+        </ul>
+      )}
+    </div>
   );
 };
 
