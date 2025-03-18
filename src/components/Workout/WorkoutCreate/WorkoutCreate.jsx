f791c48f4f223a5b71d776f4a4d4a92b195ccb5e
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2025-03-17 19:12:28 -0700

    troubleshoot workoutcreate

--- src/components/Workout/WorkoutCreate/WorkoutCreate.jsx
+++ src/components/Workout/WorkoutCreate/WorkoutCreate.jsx
@@ -4,18 +4,30 @@ import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
 const exercises = [
-  { id: 1, name: 'Bench Press', type: 'Strength', reps: [] },
-  { id: 2, name: 'Squats', type: 'Strength', reps: [] },
-  { id: 3, name: 'Sit-Ups', type: 'Strength', reps: [] },
-  { id: 4, name: 'Jump Rope', type: 'Cardio', time: [] },
-  { id: 5, name: 'Running', type: 'Cardio', time: [] },
-  { id: 6, name: 'Swimming', type: 'Cardio', time: [] },
-  { id: 7, name: 'Cycling', type: 'Cardio', time: [] },
+  { id: 'bench-press', name: 'Bench Press', type: 'Strength', sets: [] },
+  { id: 'squats', name: 'Squats', type: 'Strength', sets: [] },
+  { id: 'sit-ups', name: 'Sit-Ups', type: 'Strength', sets: [] },
+  { id: 'jump-roping', name: 'Jump Rope', type: 'Cardio', sets: [] },
+  { id: 'running', name: 'Running', type: 'Cardio', sets: [] },
+  { id: 'swimming', name: 'Swimming', type: 'Cardio', sets: [] },
+  { id: 'cycling', name: 'Cycling', type: 'Cardio', sets: [] },
 ];
 
-export default function WorkoutCreate({ workout, dispatch }) {
-  const [selectedExercise, setSelectedExercise] = useState('');
+export default function WorkoutCreate({ workouts, dispatch }) {
+  const [selectedExerciseId, setSelectedExerciseId] = useState('');
   const [workoutName, setWorkoutName] = useState('');
+
+  const createWorkout = (e) => {
+    e.preventDefault();
+    const newWorkout = {
+      id: workouts.length + 1,
+      date: new Date().toISOString(),
+      name: workoutName,
+      exercises: [],
+    };
+    dispatch({ type: 'addWorkout', payload: newWorkout });
+    setWorkoutName('');
+  };
 
   return (
     <div className={styles.createWorkout}>
@@ -35,7 +47,11 @@ export default function WorkoutCreate({ workout, dispatch }) {
             onChange={(e) => setWorkoutName(e.target.value)}
           />
           {/* Finish Button */}
-          <button type='submit' className={styles.createBtn}>
+          <button
+            type='submit'
+            className={styles.createBtn}
+            onClick={createWorkout}
+          >
             <FontAwesomeIcon icon={faSquareCheck} />
           </button>
         </div>
@@ -44,8 +60,8 @@ export default function WorkoutCreate({ workout, dispatch }) {
             name='exercise'
             id='exercise'
             className={styles.exerciseSelector}
-            value={selectedExercise}
-            onChange={(e) => setSelectedExercise(e.target.value)}
+            value={selectedExerciseId}
+            onChange={(e) => setSelectedExerciseId(e.target.value)}
           >
             <option value=''>Select Exercise</option>
             {exercises.map((exercise) => (
@@ -54,6 +70,36 @@ export default function WorkoutCreate({ workout, dispatch }) {
               </option>
             ))}
           </select>
+        </div>
+        <div className={styles.exerciseContainer}>
+          {selectedExerciseId && (
+            <div className={styles.exerciseForm}>
+              <p>Set {selectedExercise.sets.length + 1}</p>
+              <label htmlFor='reps' hidden>
+                Reps
+              </label>
+              <input
+                type='number'
+                placeholder='Reps'
+                value={
+                  selectedExercise.sets[selectedExercise.sets.length - 1].reps
+                }
+              />
+              <label htmlFor='weight' hidden>
+                Weight
+              </label>
+              <input
+                type='number'
+                placeholder='Weight'
+                value={
+                  selectedExercise.sets[selectedExercise.sets.length - 1].weight
+                }
+              />
+              <button type='submit' className={styles.addBtn}>
+                <FontAwesomeIcon icon={faSquareCheck} />
+              </button>
+            </div>
+          )}
         </div>
       </form>
     </div>
