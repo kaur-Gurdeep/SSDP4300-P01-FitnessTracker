0bc210de4a3660a7a16e0f4164b8f0255b63270a
kaur-Gurdeep <gagakaur188@gmail.com>
2025-03-20 08:48:56 -0700

    resolved merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import styles from './app.module.css';
@@ -10,22 +9,10 @@ import Register from "./components/Auth/Register";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import UserDashboard from "./components/User/UserDashboard";
-
-=======
-import { React, useReducer } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'; // Global styles
-import Home from './components/Layout/Home';
-import Footer from './components/Layout/Footer';
-import Header from './components/Layout/Header';
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
-import About from './components/About';
-import Contact from './components/Contact';
 import WorkoutHistory from './components/Workout/WorkoutHistory/WorkoutHistory';
 import WorkoutCreate from './components/Workout/WorkoutCreate/WorkoutCreate';
 import { initialWorkoutData } from './temp/data/workout.js';
->>>>>>> master
+
 
 function App() {
   const reducer = (state, action) => {
@@ -50,7 +37,6 @@ function App() {
 
   return (
     <Router>
-<<<<<<< HEAD
     <div className={styles.appContainer}>
       <Header />
       <Routes>
@@ -60,19 +46,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/user-dashboard" element={<UserDashboard />} />
-      </Routes>
-      <Footer />
-    </div>
-=======
-      <div className='appContainer'>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route
+        <Route
             path='/workouts'
             element={<WorkoutHistory workouts={workouts} />}
           />
@@ -80,10 +54,9 @@ function App() {
             path='/workout/create'
             element={<WorkoutCreate dispatch={dispatch} />}
           />
-        </Routes>
-        <Footer />
-      </div>
->>>>>>> master
+      </Routes>
+      <Footer />
+    </div>
     </Router>
   );
 }
