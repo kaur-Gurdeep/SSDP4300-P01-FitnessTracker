d8d5a8ba697829d419214166daaa15be4e0e5fd1
kaur-Gurdeep <gagakaur188@gmail.com>
2025-03-20 08:53:18 -0700

    resolved merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import styles from './app.module.css';
 import Home from './components/Layout/Home';
