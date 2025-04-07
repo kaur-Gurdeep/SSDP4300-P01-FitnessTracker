f456018f982d4953e1b75342929f0625217db7d0
Marc-Andre Baron-Campeau <m.baron.campeau@gmail.com>
2025-04-07 08:44:49 -0700

    working on delete method

--- src/components/Workout/WorkoutHistory/WorkoutHistory.jsx
+++ src/components/Workout/WorkoutHistory/WorkoutHistory.jsx
@@ -2,9 +2,7 @@ import WorkoutCard from '../WorkoutCard/WorkoutCard';
 import styles from './WorkoutHistory.module.css';
 
 export default function WorkoutHistory({ workouts }) {
-  const sortedWorkouts = [...workouts].sort(
-    (a, b) => new Date(b.date) - new Date(a.date)
-  );
+  const sortedWorkouts = [...workouts].sort((a, b) => new Date(b.date) - new Date(a.date));
 
   return (
     <div className={styles.workoutHistory}>
