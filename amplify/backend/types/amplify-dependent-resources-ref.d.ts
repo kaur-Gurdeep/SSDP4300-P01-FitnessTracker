d4518f5e6736c915099bc2ec53953c7016c5dad5
abdulsamadmaqsoudi <samad.maqsoudi@gmail.com>
2025-04-10 11:20:25 -0700

    Cognito added

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ssdp4300p01fitnesstrd467ca41": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
